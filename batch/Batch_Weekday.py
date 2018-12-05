693fb0316513668796373bbf0eda6d593b4d4bab
jrathgeber <jrathgeber@yahoo.com>
2018-12-05 07:50:26 -0500

    v little

--- batch/Batch_Weekday.py
+++ batch/Batch_Weekday.py
@@ -2,20 +2,20 @@
 
 import os;
 
-os.chdir('C:\\dep\\batch\\')
+os.chdir('C:\\dep\\Mechanizd\\batch\\')
 
 exec(open("C:\\dep\\coin\\batch.py").read())
 
 #os.chdir('C:\\dep\\rightedge\\')
 
-exec(open("C:\\dep\\rightedge\\RightEdge.py").read())
+exec(open("C:\\dep\\Mechanizd\\rightedge\\RightEdge.py").read())
 
-exec(open("C:\\dep\\rightedge\\RightEdgeIBFull.py").read())
+exec(open("C:\\dep\\Mechanizd\\rightedge\\RightEdgeIBFull.py").read())
 
 #os.chdir('C:\\dep\\zorro\\')
 
-exec(open("C:\\dep\\zorro\\BatchZorroMech0.py").read())
+exec(open("C:\\dep\\Mechanizd\\zorro\\BatchZorroMech0.py").read())
 
-exec(open("C:\\dep\\zorro\\BatchZorroMech1.py").read())
+exec(open("C:\\dep\\Mechanizd\\zorro\\BatchZorroMech1.py").read())
 
-exec(open("C:\\dep\\zorro\\BatchZorroMech2.py").read())
+exec(open("C:\\dep\\Mechanizd\\zorro\\BatchZorroMech2.py").read())
