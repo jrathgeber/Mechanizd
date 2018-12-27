bc92fb9a9d909717d9f6a4242b7c6b0eaf68a32b
jrathgeber <jrathgeber@yahoo.com>
2018-12-27 17:52:50 -0500

    zorro

--- batch/BatchMktClose.py
+++ batch/BatchMktClose.py
@@ -20,7 +20,7 @@ gdurl = config['godaddy']['url']
 gduser = config['godaddy']['user']
 gdpass = config['godaddy']['pass']
 
-os.chdir('c:\\dep\\Mechanizd\\rightedge\\')
+
 
 #cc_list = ('jrathgeber@yahoo.com','rathgeber.webster@gmail.com')
 cc_list = ('jrathgeber@yahoo.com')   
@@ -29,12 +29,16 @@ model = 'MaxAlphaLive'
 timestr = time.strftime("%d%m%y")
 session = ftplib.FTP(gdurl,gduser,gdpass)
 
+# Backtest Js and download prices    
+exec(open('C:\\dep\\Mechanizd\\zorro\\backtest.py').read())
+os.chdir('c:\\dep\\Mechanizd\\rightedge\\')
+
 #send live MaxAlpha Output
 with open('C:\dec\RightEdge\Systems\MaxAlphaLive\output.html', 'r') as f:
     data = str(f.read())
     sendMail.send_mail('jrathgeber@yahoo.com', cc_list, 'Max Alpha One Live', data, ['C:\dec\RightEdge\Systems\MaxAlphaLive\output.txt'])
     
-    #copyfile("C:\dec\RightEdge\Systems\\" + model + "\output.html", 'C:\dev\godaddy\\mech\output\RightEdge\\Live\\' + model + '_' + timestr + '.htm')  
+    copyfile("C:\dec\RightEdge\Systems\\" + model + "\output.html", 'C:\dev\godaddy\\mech\output\RightEdge\\Live\\' + model + '_' + timestr + '.htm')  
 
     file = open('C:\dec\RightEdge\Systems\MaxAlphaLive\output.html','rb')
     session.storbinary('STOR /mech/output/RightEdge/Live/' + model + '_' + timestr + '.htm', file)     # send the file
