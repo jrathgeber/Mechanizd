28cc98beb79a7c0febd214b72f6c4904b75e9005
jrathgeber <jrathgeber@yahoo.com>
2019-02-04 20:30:00 -0500

    twitter

---
+++ edu/twits.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Ty to : 
+
+# https://www.geeksforgeeks.org/tweet-using-python/
+
+import tweepy 
+import configparser
+config = configparser.ConfigParser()
+config.read('C:\etc\properties.ini') 
+
+   
+# personal details 
+consumer_key=config['twitter']['consumer_key']
+consumer_secret=config['twitter']['consumer_secret']
+
+access_token=config['twitter']['access_token']
+access_token_secret=config['twitter']['access_token_secret']
+  
+# authentication of consumer key and secret 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+  
+# authentication of access token and secret 
+auth.set_access_token(access_token, access_token_secret) 
+api = tweepy.API(auth) 
+  
+# update the status 
+api.update_status(status ="That is all for today !") 
+
