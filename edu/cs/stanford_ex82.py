d211d9598abcd020444d4e36ab973f58d4821fc9
jrathgeber <jrathgeber@yahoo.com>
2019-04-01 05:48:26 -0400

    mat

--- edu/cs/stanford_ex82.py
+++ edu/cs/stanford_ex82.py
@@ -39,3 +39,26 @@ Y
 R
 
 sns.heatmap(Y, yticklabels=False, xticklabels=False);
+
+import scipy.io #Used to load the OCTAVE *.mat files
+
+datafile = 'F:\ML\machine-learning-ex8\machine-learning-ex8\ex8\ex8_movies.mat'
+mat = scipy.io.loadmat( datafile )
+Y = mat['Y']
+R = mat['R']
+
+nm, nu = Y.shape
+# Y is 1682x943 containing ratings (1-5) of 1682 movies on 943 users
+# a rating of 0 means the movie wasn't rated
+# R is 1682x943 containing R(i,j) = 1 if user j gave a rating to movie i
+
+
+#print ('Average rating for movie 1 (Toy Story): %0.2f', % \ np.mean([ Y[0][x] for x in xrange(Y.shape[1]) if R[0][x] ]))
+
+# "Visualize the ratings matrix"
+fig = plt.figure(figsize=(6,6*(1682./943.)))
+dummy = plt.imshow(Y)
+dummy = plt.colorbar()
+dummy = plt.ylabel('Movies (%d)'%nm,fontsize=20)
+dummy = plt.xlabel('Users (%d)'%nu,fontsize=20)
+
