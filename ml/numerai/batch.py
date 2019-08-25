f3bf4521cbfbee585d277a20d48a582131dbe978
jrathgeber <jrathgeber@yahoo.com>
2019-08-25 08:44:48 -0400

    kazutsugi

--- ml/numerai/batch.py
+++ ml/numerai/batch.py
@@ -1,75 +1,56 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun May  5 12:12:26 2019
-
-@author: Jason
 
 """
 
-#!/usr/bin/env python
+Created on Saturday August 24th 2019
+
+@author: Jason Rathgeber
+
+"""
 
 import numerox as nx
 import configparser
-import numerai_xg6 as mechXg
+import numerai_kg1 as mechXg
 import os
 import time
  	
 from zipfile import ZipFile
 
-def numerox_example():
+def run_numerai_batch():
     
+    # Get Reference to Properties
     config = configparser.ConfigParser()
     config.read('C:\etc\properties.ini') 
     
+    # Numerai credentials for submission
     public = config['numerai']['public']
     secret = config['numerai']['secret']
     
     
-    """
-    Example of how to prepare a submission for the Numerai tournament.
-    It uses Numerox which you can install with: pip install numerox
-    For more information see: https://github.com/kwgoodman/numerox
-    """
-    
-    #contest = str(165)
-    #contest = str(config['numerai']['tournament'])
+    # compute the tournament numer
     week = time.strftime("%U")
     contest = str(int(week) + 140)
 
+    # Work out directory and create if not exists
     directory = 'F:\\Numerai\\numerai' + contest + '\\'
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-        # download dataset from numerai, save it and then load it
-        # data = nx.download(directory + 'numerai_dataset.zip')
-        nx.download(directory + 'numerai_dataset.zip')
+    # download dataset from numerai, save it and then load it
+    nx.download(directory + 'numerai_dataset.zip')
 
-        with ZipFile(directory + 'numerai_dataset.zip', 'r') as zipObj:
-            #Extract all the contents of zip file in current directory
-            zipObj.extractall(directory)
+    # Unzip it
+    with ZipFile(directory + 'numerai_dataset.zip', 'r') as zipObj:
+        #Extract all the contents of zip file in current directory
+        zipObj.extractall(directory)
 
-    # we will use logistic regression; you will want to write your own model
-    # model = nx.logistic()
-
-    # fit model with train data and make predictions for tournament data
-    #prediction = nx.production(model, data, tournament='bernie')
-
-    # save predictions to csv file
-    # prediction.to_csv('logistic.csv', verbose=True)
-
+    # Run my xg boost algo on it
     mechXg.main(contest)
 
-    # upload predictions to Numerai to enter the tournament
-    # create the public_id and secret_key on the Numerai website
-    #
-    # nx.upload('logistic.csv', tournament='bernie', public_id, secret_key)
-
-    names = ('bernie', 'ken', 'charles', 'frank', 'hillary') 
-    #names = ('hillary',)     
-    
+    # Upload to numerai
+    names = ('kazutsugi',)     
     for name in names:
-
         nx.upload(directory + name + '_new_submission.csv', name, public, secret)
     
 
@@ -78,5 +59,5 @@ if __name__ == '__main__':
     week = time.strftime("%U")
     contest = int(week) + 140
     print(str(contest))
-    numerox_example()
+    run_numerai_batch()
 
