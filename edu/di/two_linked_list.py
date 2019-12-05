6285181da219968110b2eb78e9354abaf643dca4
jrathgeber <jrathgeber@yahoo.com>
2019-12-05 08:34:20 -0500

    interv

---
+++ edu/di/two_linked_list.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 14:31:02 2019
+
+@author: Jason
+
+You are given two linked-lists representing two non-negative integers. 
+
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+
+Add the two numbers and return it as a linked list.
+
+
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+  def addTwoNumbers(self, l1, l2, c = 0):
+    # Fill this in.
+
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+                
+        while l1 or l2 or carry:            
+            val1  = (l1.val if l1 else 0)
+            val2  = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2 + carry, 10)    
+                      
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next                      
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+               
+        return result.next
+    
+
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = Solution().addTwoNumbers(l1, l2)
+while result:
+  print (result.val),
+  result = result.next
+# 7 0 8
