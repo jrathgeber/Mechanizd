9179677a63215875522e1c061e630ccf2a8b77b1
jrathgeber <jrathgeber@yahoo.com>
2020-10-12 20:23:49 -0400

    updated

---
+++ edu/gc/auth.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 10:31:12 2020
+
+@author: Jason
+"""
+
+
+from google.cloud import storage
+
+# If you don't specify credentials when constructing the client, the
+# client library will look for credentials in the environment.
+storage_client = storage.Client()
+
+# Make an authenticated API request
+buckets = list(storage_client.list_buckets())
+print(buckets)
---
+++ edu/gc/numerai/numerai.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 14:59:51 2020
+
+@author: Jason
+"""
+
