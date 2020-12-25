6e86a45cda1a55b066925ba9ead4f91b90450942
jrathgeber <jrathgeber@yahoo.com>
2020-12-25 18:26:44 -0500

    christmas

---
+++ ml/azure/03-run-hello.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 11:40:30 2020
+
+@author: Jason
+"""
+
+# tutorial/03-run-hello.py
+from azureml.core import Workspace, Experiment, Environment, ScriptRunConfig
+
+ws = Workspace.from_config()
+experiment = Experiment(workspace=ws, name='day1-experiment-hello')
+
+config = ScriptRunConfig(source_directory='./src', script='hello.py', compute_target='cpu-cluster')
+
+run = experiment.submit(config)
+aml_url = run.get_portal_url()
+print(aml_url)
