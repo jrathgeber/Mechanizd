d4023743f8534cf3f4239c0e87c4d1356fe9ee00
jrathgeber <jrathgeber@yahoo.com>
2020-12-26 19:47:04 -0500

    Boxing Day 4

---
+++ ml/numerai_azure/02-create-compute.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 12:19:15 2020
+
+@author: Jason
+"""
+
+# tutorial/02-create-compute.py
+from azureml.core import Workspace
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+
+ws = Workspace.from_config() # This automatically looks for a directory .azureml
+
+# Choose a name for your CPU cluster
+cpu_cluster_name = "cpu-cluster"
+
+# Verify that the cluster does not exist already
+try:
+    cpu_cluster = ComputeTarget(workspace=ws, name=cpu_cluster_name)
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_D2_V2',
+                                                           idle_seconds_before_scaledown=2400,
+                                                           min_nodes=0,
+                                                           max_nodes=4)
+    cpu_cluster = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
+
+cpu_cluster.wait_for_completion(show_output=True)
---
+++ ml/numerai_azure/src/hello.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 11:04:49 2020
+
+@author: Jason
+"""
+
+# src/hello.py
+print("Hello world!")
