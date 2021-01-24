5dd8dd6a665261632602f3558de6e0756d1e5b4c
jrathgeber <jrathgeber@yahoo.com>
2021-01-23 23:09:29 -0500

    any

--- ml/numerai/numerai_nomi2_tune.py
+++ ml/numerai/numerai_nomi2_tune.py
@@ -25,10 +25,10 @@ warnings.filterwarnings("ignore")
 print("\n Loading Numerai Data...")
 
 # The training data is used to train your model how to predict the targets.
-train = pd.read_csv('F:\\Numerai\\numerai' + contest + '\\numerai_training_data.csv', header=0)
+train = pd.read_csv('F:\\Numerai\\numerai' + str(247) + '\\numerai_training_data.csv', header=0)
 
 # The tournament data is the data that Numerai uses to evaluate your model.
-tournament = pd.read_csv('F:\\Numerai\\numerai'+ contest +'\\numerai_tournament_data.csv', header=0)
+tournament = pd.read_csv('F:\\Numerai\\numerai'+ str(247) +'\\numerai_tournament_data.csv', header=0)
 
 # The tournament data contains validation data, test data and live data.
 # Validation is used to test your model locally so we separate that.
