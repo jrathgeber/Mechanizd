ba1aa49fe3c37870ce1a1baad3b77166e8374064
jrathgeber <jrathgeber@yahoo.com>
2021-02-02 20:04:50 -0500

    hallo

--- ml/numerai_azure/05-run-numerai-nomi.py
+++ ml/numerai_azure/05-run-numerai-nomi.py
@@ -17,11 +17,11 @@ if __name__ == "__main__":
     datastore = ws.get_default_datastore()
     dataset = Dataset.File.from_files(path=(datastore, 'datasets/numerai'))
 
-    experiment = Experiment(workspace=ws, name='day14-experiment-nomi-2')
+    experiment = Experiment(workspace=ws, name='day15-experiment-nomi-1')
 
     config = ScriptRunConfig(
         source_directory='./src',
-        script='train_nomi_2.py',
+        script='train_nomi_1.py',
         compute_target='cpu-cluster3',
         arguments=[
             '--data_path', dataset.as_named_input('input').as_mount(),
