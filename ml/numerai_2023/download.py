a2ae83fde9904d3f4267cf698710d02732bebb93
jrathgeber <jrathgeber@yahoo.com>
2023-04-06 06:02:13 -0400

    example

--- ml/numerai_2023/download.py
+++ ml/numerai_2023/download.py
@@ -4,10 +4,14 @@ from numerapi import NumerAPI
 
 napi = NumerAPI()
 
-napi.download_current_dataset("v4.1/train.parquet", "train.parquet")
-napi.download_current_dataset("v4.1/validation.parquet", "validation.parquet")
-napi.download_current_dataset("v4.1/live.parquet", "live.parquet")
-napi.download_current_dataset("v4.1/live_example_preds.parquet", "live_example_preds.parquet")
-napi.download_current_dataset("v4.1/validation_example_preds.parquet", "validation_example_preds.parquet")
-napi.download_current_dataset("v4.1/features.json", "features.json")
-napi.download_current_dataset("v4.1/meta_model.parquet", "meta_model.parquet")
+
+contest = str(500)
+directory = 'F:\\Numerai\\numerai' + contest + '\\'
+
+napi.download_current_dataset(directory + "v4.1\\train.parquet", "train.parquet")
+napi.download_current_dataset(directory + "v4.1\\validation.parquet", "validation.parquet")
+napi.download_current_dataset(directory + "v4.1\\live.parquet", "live.parquet")
+napi.download_current_dataset(directory + "v4.1\\live_example_preds.parquet", "live_example_preds.parquet")
+napi.download_current_dataset(directory + "v4.1\\validation_example_preds.parquet", "validation_example_preds.parquet")
+napi.download_current_dataset(directory + "v4.1\\features.json", "features.json")
+napi.download_current_dataset(directory + "v4.1\\meta_model.parquet", "meta_model.parquet")
