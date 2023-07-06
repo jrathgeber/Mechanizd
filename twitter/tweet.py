b8c421169d450c4dfb29ef075a2ee9f4b4203110
Jason Rathgeber <jrathgeber@yahoo.com>
2023-07-05 21:04:31 -0400

    tweep2

--- twitter/tweet.py
+++ twitter/tweet.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Ty to : 
-
 # https://www.geeksforgeeks.org/tweet-using-python/
 
 import tweepy 
@@ -11,25 +9,29 @@ config.read('C:\etc\properties.ini')
 
    
 def tweetSomething(something):
-      
+
+
+
     # personal details 
     consumer_key=config['twitter']['consumer_key']
     consumer_secret=config['twitter']['consumer_secret']
-    
     access_token=config['twitter']['access_token']
-    access_token_secret=config['twitter']['access_token_secret']
-      
-    # authentication of consumer key and secret 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-      
-    # authentication of access token and secret 
-    auth.set_access_token(access_token, access_token_secret) 
-    api = tweepy.API(auth) 
-      
-    # update the status 
-    #api.update_status(status ="Penny Stock Watchlist for today : $TMSR $AMRS $CRON") 
-    #api.update_status(status ="Happy Presidents Day") 
-    api.update_status(status = something)
+    access_token_secret = config['twitter']['access_token_secret']
+
+    bearer_token = config['twitter']['bearer_token']
+
+    #client = tweepy.Client(bearer_token=bearer_token)
+
+    client = tweepy.Client(bearer_token=bearer_token,
+                            consumer_key=consumer_key,
+                           consumer_secret=consumer_secret,
+                           access_token=access_token,
+                           access_token_secret=access_token_secret)
+
+    # Replace the text with whatever you want to Tweet about
+    response = client.create_tweet(text='hello world from API V2 !')
+
+    print(response)
 
 
 if __name__ == '__main__':
