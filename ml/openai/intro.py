19c44a1dd827de8063abef978348095669e31285
jrathgeber <jrathgeber@yahoo.com>
2023-09-26 20:15:40 -0400

    Little openai

--- ml/openai/intro.py
+++ ml/openai/intro.py
@@ -24,7 +24,7 @@ openai.Model.list()
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+    {"role": "user", "content": "Write me a short punchy inspirational tweet"}
   ]
 )
 
