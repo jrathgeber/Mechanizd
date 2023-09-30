744aa80f4b8f11468312ab1a24a255fc4a04df18
jrathgeber <jrathgeber@yahoo.com>
2023-09-30 16:54:20 -0400

    neet

---
+++ edu/py/neet.py
@@ -0,0 +1,14 @@
+myMap = {"matteo": 1,"luca2":2, "fabio":3 }
+
+for key in myMap:
+    print(key, myMap[key])
+
+for val in myMap.values():
+    print(val)
+
+for key, val in myMap.items():
+    print(key, val)
+
+
+tup = (1,2,3)
+print(tup)
