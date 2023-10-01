fad28525f4182b6955947bbaed957db244709166
jrathgeber <jrathgeber@yahoo.com>
2023-10-01 18:01:02 -0400

    the basics

---
+++ edu/py/variables.py
@@ -0,0 +1,23 @@
+n, m = 0, "abc";
+
+print(n)
+print ('n=', n)
+
+n= n+1;
+n += 1;
+#n++; bad
+
+n="abc"
+
+print('n=', n)
+
+n=4
+n = None # ok
+
+# if
+
+if n>6:
+    print("its bigger")
+elif n == 19:
+    print("volia")
+
