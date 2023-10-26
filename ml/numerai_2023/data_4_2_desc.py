c7ad289a10cfc862c69acb0cf80174dedf6efd5f
jrathgeber <jrathgeber@yahoo.com>
2023-10-25 21:36:07 -0400

    describe

--- ml/numerai_2023/data_4_2_desc.py
+++ ml/numerai_2023/data_4_2_desc.py
@@ -12,3 +12,19 @@ df = pandas.read_parquet(
 
 print("df.shape")
 print(df.shape)
+# print(df.describe())
+
+# Get the list of all column names from headers
+column_names = list(df.columns.values)
+print("df.shape")
+print(column_names)
+
+
+eras = df.era
+print ("") # There are 120 eras numbered from 1 to 120
+print ("eras.describe()")
+print (eras.describe())
+
+print ("") # The earlier eras are smaller, but generally each era is 4000-5000 rows
+print ("df.groupby(eras).size().plot()")
+df.groupby(eras).size().plot()
