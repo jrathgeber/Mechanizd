8757b0836054cac46547a420d4ff98d45141a228
jrathgeber <jrathgeber@yahoo.com>
2023-11-06 22:24:03 -0500

    profile

---
+++ edu/pd/profile.py
@@ -0,0 +1,11 @@
+## pip install pandas-profiling
+
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+# Load the dataset
+#data = pd.read_csv("titanic.csv")
+
+# Perform EDA using pandas_profiling
+#profile = ProfileReport(data)
+#profile.to_file("titanic_report.html")
