0d9bc1ec31366d9cf31989f42ff7571e50e8724b
Jason Rathgeber <jrathgeber@yahoo.com>
2025-01-25 23:57:37 -0500

    image2

--- content/Blog.py
+++ content/Blog.py
@@ -15,7 +15,10 @@ file_path_prod = "C:\\dev\\godaddy\\vcard\\blogpost\\Articles\\"
 file_path_laptop = "D:\\gd23\\vcard\\blogpost\\Articles\\"
 file_path_laptop_bp = "D:\\gd23\\vcard\\blogpost\\"
 
+file_path_laptop_image = "D:\\gd23\\vcard\\assets\\custom\\images\\blog\\"
+file_path_laptop_thumb = "D:\\gd23\\vcard\\assets\\custom\\images\\blog\\"
+
 #Article.new_article(file_path_laptop, article_number, slug)
 #Post.new_post(file_path_laptop_bp, article_number, slug, key_words)
 #IndexPage.add_blog(file_path_laptop_bp, article_number, slug, key_words, "Jan22")
-Image.new_image(file_path_laptop, article_number, slug, key_words, "Jan22")
+Image.new_image(file_path_laptop_image, file_path_laptop_thumb, article_number, slug, key_words, "Jan22")
--- content/BlogImage.py
+++ content/BlogImage.py
@@ -13,10 +13,12 @@ def download_image(url, file_name):
         print("Failed to retrieve the image")
 
 
-def new_image(file_path_laptop, article_number, slug, key_words, ddate):
+def new_image(file_path_laptop_image, file_path_laptop_thumb, article_number, slug, key_words, ddate):
 
-    image_url = ai.create_image(file_path_laptop, article_number, slug, key_words, ddate)
+    image_url = ai.create_image(file_path_laptop_image, article_number, slug, key_words, ddate)
 
-    file_name = 'zappy\\' + article_number + '_' + slug + '.jpg'
+    file_name = file_path_laptop_image + article_number + '_' + slug + '.jpg'
+    file_name_thumb = file_path_laptop_thumb  + article_number + '_' + slug + '.jpg'
 
     download_image(image_url, file_name)
+    download_image(image_url, file_name_thumb)
