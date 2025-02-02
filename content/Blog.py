14c131cb790912edf524b7b3db0858488bcdc0b9
Jason Rathgeber <jrathgeber@yahoo.com>
2025-02-02 14:32:51 -0500

    blogs are working

--- content/Blog.py
+++ content/Blog.py
@@ -4,8 +4,8 @@ import BlogPost as Post
 import BlogIndex as IndexPage
 import BlogImage as Image
 
-article_number = "031"
-key_words = "The journey of leadership"
+article_number = "033"
+key_words = "What is SSO"
 
 further_info = """
 
@@ -132,10 +132,10 @@ file_path_laptop = "D:\\gd23\\vcard\\blogpost\\Articles\\"
 file_path_laptop_bp = "D:\\gd23\\vcard\\blogpost\\"
 
 file_path_laptop_image = "D:\\gd23\\vcard\\assets\\custom\\images\\blog\\"
-file_path_laptop_thumb = "D:\\gd23\\vcard\\assets\\custom\\images\\blog\\"
+file_path_laptop_thumb = "D:\\gd23\\vcard\\assets\\custom\\images\\blog\\thumbs\\"
 
 Article.new_article(file_path_laptop, article_number, slug, key_words, further_info)
 Post.new_post(file_path_laptop_bp, article_number, slug, key_words)
-Image.new_image(file_path_laptop_image, file_path_laptop_thumb, article_number, slug, key_words, "Jan22")
-IndexPage.add_blog(file_path_laptop_bp, article_number, slug, key_words, "Feb 01, 2025")
+Image.new_image(file_path_laptop_image, file_path_laptop_thumb, article_number, slug, key_words, "Feb 02, 2025")
+IndexPage.add_blog(file_path_laptop_bp, article_number, slug, key_words, "Feb 02, 2025")
 
