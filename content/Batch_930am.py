3daaed349f3a56ad18ff880f449e9a13ce4e44f3
Jason Rathgeber <jrathgeber@yahoo.com>
2025-02-24 22:10:44 -0500

    mail

--- content/Batch_930am.py
+++ content/Batch_930am.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
-import time;
-import sendMail;
-import threading;
+import time
+import content.email.sendMail as sm
+import threading
 import configparser
-import Tweet
+import content.twitter.tweet
 
 config = configparser.ConfigParser()
-config.read('C:\etc\properties.ini') 
+config.read('C:\\etc\\properties.ini')
 
 gdurl = config['godaddy']['godaddy.url']
 gduser = config['godaddy']['godaddy.user']
@@ -21,7 +21,7 @@ server = config['yahoo']['yahoo.server']
 port = config['yahoo']['yahoo.port']
 username = config['yahoo']['yahoo.username']
 
-os.chdir('C:\\dep\Mechanizd\\maxalpha\\')
+#os.chdir('C:\\dep\\Mechanizd\\maxalpha\\')
 
 daterun = time.strftime("%Y%m%d")
 
@@ -37,8 +37,6 @@ mode = ''
 maxdata='hello'
 
 
-
-    
 tickerList = ['AXSM', 'VIPS']
 #tickerList = MA.parseWebSite(daterun);
 print(tickerList)
@@ -47,10 +45,10 @@ print(tickerList)
 #print(allpriceslist)
 
 if tickerList[0]=='error':
-    sendMail.send_mail('jrathgeber@yahoo.com', 'jrathgeber@yahoo.com', 'Max List Eorror ' + ''.join(tickerList), ''.join(tickerList), [], server, port, username, password)
+    sm.send_mail('jrathgeber@yahoo.com', 'jrathgeber@yahoo.com', 'Max List Eorror ' + ''.join(tickerList), ''.join(tickerList), [], server, port, username, password)
 else:
     # SC.getSymbolConfig(tickerList, daterun, maxdata)
-    #sendMail.send_mail(user, user, 'Max ' + ','.join(tickerList), ', $'.join(tickerList) + ' \n \n Brought to you by https://www.mechanizd.com',[], server, port, username, password)
+    sm.send_mail(user, user, 'Max ' + ','.join(tickerList), ', $'.join(tickerList) + ' \n \n Brought to you by https://www.mechanizd.com',[], server, port, username, password)
     #Tweet.tweetSomething('Equity day trade algo focus list $' + ', $'.join(tickerList) + ' \n \n Brought to you by https://www.mechanizd.com')
     #sendMail.send_mail(user, user, 'Max ' + ','.join(tickerList), ', $'.join(tickerList) + '',[], server, port, username, password)
     #Tweet.tweetSomething('Equity day trade algo focus list $' + ', $'.join(tickerList) + '')
