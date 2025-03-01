d61684afd135b879a6f95056072a23cad7b4ce69
jrathgeber <jrathgeber@yahoo.com>
2025-03-01 15:08:45 -0500

    yt upload

---
+++ content/Batch_Sat_Solo.py
@@ -0,0 +1,10 @@
+
+
+cmd = '''
+
+python3 upload_video.py --file="F:\\Photos\\Videos_2024\\20241017_151954.mp4" --title="Goats on the roof" --description="Goats on the roof Coombs BC Canada" --keywords="Goats on roof" --category="22" --privacyStatus="private"
+
+C:\\Users\\Jason\\anaconda3\\envs\\Mechanizd\\python.exe C:\\dep\\Mechanizd\\content\\youtub\\upload_video.py --file="F:\\Photos\\Videos_2024\\20241017_151954.mp4" --title="Goats on the roof" --description="Goats on the roof Coombs BC Canada" --keywords="Goats on roof" --category="22" --privacyStatus="private"
+
+
+'''
--- content/youtub/upload_video.py
+++ content/youtub/upload_video.py
@@ -40,7 +40,7 @@ RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 #   https://developers.google.com/youtube/v3/guides/authentication
 # For more information about the client_secrets.json file format, see:
 #   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-CLIENT_SECRETS_FILE = "client_secrets.json"
+CLIENT_SECRETS_FILE = "C:\\etc\\client_secrets.json"
 
 # This OAuth 2.0 access scope allows an application to upload files to the
 # authenticated user's YouTube channel, but doesn't allow other types of access.
@@ -161,24 +161,21 @@ def resumable_upload(insert_request):
 
 
 if __name__ == '__main__':
-    argparser.add_argument("--file", required=True,
-                           help="Video file to upload")
+
+    argparser.add_argument("--file", required=True, help="Video file to upload")
     argparser.add_argument("--title", help="Video title", default="Test Title")
-    argparser.add_argument("--description", help="Video description",
-                           default="Test Description")
-    argparser.add_argument("--category", default="22",
-                           help="Numeric video category. " +
-                           "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
-    argparser.add_argument("--keywords", help="Video keywords, comma separated",
-                           default="")
-    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
-                           default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
+    argparser.add_argument("--description", help="Video description", default="Test Description")
+    argparser.add_argument("--category", default="22", help="Numeric video category. " + "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
+    argparser.add_argument("--keywords", help="Video keywords, comma separated", default="")
+    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES, default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
+
     args = argparser.parse_args()
 
     if not os.path.exists(args.file):
         exit("Please specify a valid file using the --file= parameter.")
 
     youtube = get_authenticated_service(args)
+
     try:
         initialize_upload(youtube, args)
     except HttpError as e:
