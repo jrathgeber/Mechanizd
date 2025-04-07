a688aa8517f94fd4ba4e8a0363dfbf299cf8c679
jrathgeber <jrathgeber@yahoo.com>
2025-04-07 06:39:59 -0400

    ithaca 8

--- content/prompts/aGainers.py
+++ content/prompts/aGainers.py
@@ -5,11 +5,13 @@ def get_prompt(ticker_list):
     
 Provide super concise info about the following stocks. 
 
-If a stock can't be found just ignore it and say nothing. 
+If a stock can't be found just ignore it and say nothing. Don't say not found.  
 
-Don't provide citation numbers. 
+Don't provide citation numbers. Don't use asterisk or dashes.
 
 Don't introduce the list just get to the stocks. 
+
+Say thanks for reading at the end. Something like "Follow me for stock and financial tweets". Change the ending every time. 
         
     """ + ticker_list
 
